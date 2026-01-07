bb3efb050f249c7abb1728264cbe6c55cbc3c56f
Renan Fabiani <160786426+RenanFaAl@users.noreply.github.com>
2026-01-06 21:47:05 -0300

    Commit

--- estruturaDados/Recursividade.java
+++ estruturaDados/Recursividade.java
@@ -6,7 +6,17 @@ public class Recursividade {
 		// TODO Auto-generated method stub
 		int produto;
 		produto=ex3(4,2);
+		
+		int fibo;
+		fibo = fibonacci(5);
+		
+		int fat;
+		fat = fatorial(5);
+		
+		
 		System.out.println(produto);
+		System.out.println(fibo);
+		System.out.println(fat);
 	}
 
 	private static int ex3(int i, int j) {
@@ -18,4 +28,20 @@ public class Recursividade {
 		return j+ ex3(i-1,j);
 	}
 
+	private static int fibonacci(int i) {
+		if(i == 1) {
+			return 1;
+		}
+		if (i == 0) {
+			return 0;
+		}
+		return fibonacci(i-1)+ fibonacci(i-2);
+	}
+	
+	private static  int fatorial(int i) {
+		if (i <= 1) {
+			return 1;
+		}
+		return i * fatorial(i-1);
+	}
 }
